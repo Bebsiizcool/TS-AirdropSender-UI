0400e05360a7ca660a7b4e569b945a2b02cb8d7c
bebsiizcool <basilshahzad18@gmail.com>
2026-04-18 23:03:22 +0500

    vantra

---
+++ my-app/src/types/vanta.d.ts
@@ -0,0 +1,14 @@
+declare module 'vanta/dist/vanta.globe.min' {
+  const GLOBE: any
+  export default GLOBE
+}
+
+declare module 'vanta/dist/vanta.net.min' {
+  const NET: any
+  export default NET
+}
+
+declare module 'vanta/dist/vanta.fog.min' {
+  const FOG: any
+  export default FOG
+}
